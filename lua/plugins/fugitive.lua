a5f5d125d48a3bbd398899b7e2901bb1fdc6ad2c
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2026-01-21 21:12:32 -0500

    feat: Fugetive

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,15 @@
+return {
+  "tpope/vim-fugitive",
+  -- The plugin will only load when you type one of these in the command line
+  cmd = {
+    "G",
+    "Git",
+    "Gdiffsplit",
+    "Gread",
+    "Gwrite",
+    "Ggrep",
+    "GMove",
+    "GDelete",
+    "GBrowse",
+  },
+}
